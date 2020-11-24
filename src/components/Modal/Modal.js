b508a603af162f6def1e34cdee9840959c7e2177
wangzhen <thewhitelelphant@163.com>
2020-11-24 23:37:22 +0800

    add visible to modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Modal.scss';
 import classnames from 'classnames';
+import Portal from '../Portal/Portal';
 
 const Modal = ({
+    visible = false,
     children,
     title,
     footer,
@@ -16,7 +18,7 @@ const Modal = ({
     scrollable,
 }) => {
     let modalWrapperCls = classnames(
-        { 
+        {
             "modal__wrapper--centered": centered,
             "modal__wrapper--scrollable": scrollable
         }
@@ -24,26 +26,29 @@ const Modal = ({
     let modalContentStyle = {
         ...modalStyle,
     }
-    if(Number.isInteger(height)) modalContentStyle.height = height;
-    if(Number.isInteger(width)) modalContentStyle.width = width;
+    if (Number.isInteger(height)) modalContentStyle.height = height;
+    if (Number.isInteger(width)) modalContentStyle.width = width;
 
     return (
-        <div className="modal">
-            <div className={`modal__mask ${maskClsName || ""}`} style={maskStyle}></div>
-            <div className={`modal__wrapper ${modalWrapperCls}`}>
-                <div className={`modal__content ${modalClsName || ""}`} style={modalContentStyle}>
-                    <div className="modal__header">
-                        <span>{title}</span>
-                    </div>
-                    <div className="modal__body">
-                        {children}
-                    </div>
-                    <div className="modal__footer">
-                        <button>{footer}</button>
+        visible && <Portal>
+            <div className="modal">
+                <div className={`modal__mask ${maskClsName || ""}`} style={maskStyle}></div>
+                <div className={`modal__wrapper ${modalWrapperCls}`}>
+                    <div className={`modal__content ${modalClsName || ""}`} style={modalContentStyle}>
+                        <div className="modal__header">
+                            <span>{title}</span>
+                        </div>
+                        <div className="modal__body">
+                            {children}
+                        </div>
+                        <div className="modal__footer">
+                            <button>{footer}</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Portal>
+
     )
 }
 
